63af868d3cdc013443626e8664d59e9d0e7e6962
thebasementguide2026 <pnason4@gmail.com>
2026-03-15 06:34:48 -0400

    Add breadcrumbs to septic-to-sewer-conversion-cost

--- app/cost-guides/septic-to-sewer-conversion-cost/page.tsx
+++ app/cost-guides/septic-to-sewer-conversion-cost/page.tsx
@@ -1,4 +1,5 @@
 import Link from 'next/link'
+import Breadcrumbs from '@/components/Breadcrumbs'
 import LeadForm from '@/components/LeadForm'
 import Image from 'next/image'
 import type { Metadata } from 'next'
@@ -140,6 +141,12 @@ export default function SepticToSewerConversionCostPage() {
           </div>
         </div>
       </section>
+
+      <Breadcrumbs items={[
+        { label: 'Home', href: '/' },
+        { label: 'Cost Guides', href: '/cost-guides' },
+        { label: 'Septic to Sewer Conversion Cost' }
+      ]} />
 
       {/* Article Body */}
       <article className="max-w-4xl mx-auto px-4 py-12">
