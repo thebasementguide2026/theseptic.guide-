4f134859e289642b3921624023882f9b3a5ff1a2
thebasementguide2026 <pnason4@gmail.com>
2026-03-15 06:19:25 -0400

    Add breadcrumbs to septic-tank-pumping-cost

--- app/articles/septic-tank-pumping-cost/page.tsx
+++ app/articles/septic-tank-pumping-cost/page.tsx
@@ -1,4 +1,5 @@
 import Link from 'next/link'
+import Breadcrumbs from '@/components/Breadcrumbs'
 import LeadForm from '@/components/LeadForm'
 import Image from 'next/image'
 import type { Metadata } from 'next'
@@ -122,6 +123,8 @@ export default function SepticPumpingCostArticle() {
           </div>
         </div>
       </section>
+
+      <Breadcrumbs items={[{ label: 'Home', href: '/' }, { label: 'Articles', href: '/articles' }, { label: 'Septic Tank Pumping Cost' }]} />
 
       {/* Main Content */}
       <article className='max-w-4xl mx-auto px-4 py-12'>
