757aeb97b21cd17ecec966722cf1c565d52acb58
thebasementguide2026 <pnason4@gmail.com>
2026-03-15 06:40:04 -0400

    Add breadcrumbs to best-drain-cleaners-for-septic

--- app/reviews/best-drain-cleaners-for-septic/page.tsx
+++ app/reviews/best-drain-cleaners-for-septic/page.tsx
@@ -1,4 +1,5 @@
 import Link from 'next/link'
+import Breadcrumbs from '@/components/Breadcrumbs'
 import LeadForm from '@/components/LeadForm'
 import Image from 'next/image'
 import type { Metadata } from 'next'
@@ -159,6 +160,12 @@ export default function BestDrainCleanersPage() {
           </div>
         </div>
       </section>
+
+      <Breadcrumbs items={[
+        { label: 'Home', href: '/' },
+        { label: 'Reviews', href: '/reviews' },
+        { label: 'Best Drain Cleaners for Septic' }
+      ]} />
 
       {/* Article Body */}
       <article className="max-w-4xl mx-auto px-4 py-16">
