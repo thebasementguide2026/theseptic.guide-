6ba051ed61ab262598036c37c0ff2b246eb6d77c
thebasementguide2026 <pnason4@gmail.com>
2026-03-16 13:06:45 -0400

    fix drain cleaners link in header nav

--- components/Header.tsx
+++ components/Header.tsx
@@ -30,7 +30,7 @@ const navItems = [
       { label: 'Tank Risers', href: '/reviews/best-septic-tank-risers' },
       { label: 'System Alarms', href: '/reviews/best-septic-system-alarms' },
       { label: 'TP for Septic', href: '/reviews/best-toilet-paper-for-septic' },
-      { label: 'Drain Cleaners', href: '/reviews/best-drain-cleaners' },
+      { label: 'Drain Cleaners', href: '/reviews/best-drain-cleaners-for-septic' },
               { label: 'Laundry Detergent', href: '/reviews/best-septic-safe-laundry-detergent' },
     ],
   },
