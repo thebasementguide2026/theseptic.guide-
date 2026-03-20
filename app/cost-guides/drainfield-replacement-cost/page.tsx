71d3589dd2639bc1ff73e598e8089825ea269dda
thebasementguide2026 <pnason4@gmail.com>
2026-03-19 22:56:18 -0400

    Add missing Link import from next/link

--- app/cost-guides/drainfield-replacement-cost/page.tsx
+++ app/cost-guides/drainfield-replacement-cost/page.tsx
@@ -1,4 +1,5 @@
 import Image from 'next/image'
+import Link from 'next/link'
 import type { Metadata } from 'next'
 import Breadcrumbs from '@/components/Breadcrumbs'
 import LeadForm from '@/components/LeadForm'
