86fe76645d7c99ddf1e23afac064664ab9cb3d63
thebasementguide2026 <pnason4@gmail.com>
2026-03-24 11:29:53 +0000

    Add mound septic system to footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -16,6 +16,7 @@ const footerSections = [
       { label: 'Tank Size Guide', href: '/guides/septic-tank-size-guide' },
       { label: 'How Long Systems Last', href: '/articles/how-long-does-a-septic-system-last' },
       { label: 'Septic Inspection Guide', href: '/guides/septic-inspection-guide' },
+      { label: 'Mound Septic Systems', href: '/guides/mound-septic-system' },
     ],
   },
   {
